caa9e649177ec9f426e1ef8cea39a81a8acfcd74
Elod Csirmaz <csirmazelodpal@gmail.com>
2015-12-16 00:03:53 +0000

    Added licence to the script

--- 2015/prove.py
+++ 2015/prove.py
@@ -3,7 +3,7 @@ import sys
 
 """
 This script is part of the paper available at
-...
+https://github.com/csirmaz/AlgebraicSyncPaper
 
 It is used to test pairs of commands on filesystems and determine
 (1) if they break all filesystems, 
@@ -47,6 +47,27 @@ BR - the command pair breaks all filesystems (for (1) and (3))
 
 Finally, the script prints rules for substituting command pairs with single commands
 whenever the single command is not "break".
+
+
+Copyright (c) 2015 Elod Pal Csirmaz
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
 
 # CONFIGURATION STARTS
